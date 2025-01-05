ef8f7e0c4d730eabe75c6f34c53a01978989835c
Pankaj-Singh-Rawat <qoolphoenix@gmail.com>
2025-01-05 20:03:45 +0530

    doing questions

---
+++ C++/day2questions.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std ;
+
+int main (){
+//     Input: arr[] = {1, 4, 3, 2, 6, 5}  
+//     Output: {5, 6, 2, 3, 4, 1}
+
+    // int arr[] = {1, 4, 3, 2, 6, 5};
+    // int length = sizeof(arr)/ sizeof(arr[0]);
+    // int temp[length] = {};
+    // for (int i =0 ; i <= length ; i++){
+    //     temp[i] = arr[length - i - 1];
+    // }   
+    
+    // for (int i = 0; i < length; i++)
+    // {
+    //     cout << temp[i] << endl ; 
+    // }
+    
+    
+}
